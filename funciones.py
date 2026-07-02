a99888a99e8c9375996a04c2f8d040a0ecdc8097
ProfeRicvrdo <ri.yaneza@profesor.duoc.cl>
2026-07-02 15:09:55 -0400

    elimine funciones que no se usaban

--- funciones.py
+++ funciones.py
@@ -1,11 +1,5 @@
 def saludar():
     print("Este es un saludo desde mi repositorio local")
 
-def despedir():
-    print("chao")
 
-def hola():
-    print("cualkquie cosa")
 
-def buscarPorNombre(nombre):
-    #este es un comentario xd
